34d25a62c17c8d266dff4543960ceb07742f4871
lytran14 <139949514+lytran14@users.noreply.github.com>
2024-10-17 02:10:32 +0700

    update dev

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
-import { Routes, Route } from 'react-router-dom';
-import './App.scss';
-import './components/css/Product.css';
-import './components/css/ProductDetail.css';
-import './components/css/ProductManagement.css';
-import Header from './components/page/Header';
-import Banner from './components/Banner';
-import Footer from './components/page/Footer';
-import About from './components/About_us';
-import Product from './components/Product';
-import Cart from './components/Cart';
-import Contact from './components/Contact';
-import Pay from './components/Pay';
-
-import ProductDetail from './components/ProductDetail';
-import ProductManagement from './components/seller/ProductManagement';
-import CategoryManagement from './components/seller/CategoryManagement';
-
+import { Routes, Route } from "react-router-dom";
+import "./App.scss";
+import "./components/css/Product.css";
+import "./components/css/ProductDetail.css";
+import "./components/css/ProductManagement.css";
+import Header from "./components/page/Header";
+import Banner from "./components/Banner";
+import Footer from "./components/page/Footer";
+import About from "./components/About_us";
+import Product from "./components/Product";
+import Cart from "./components/Cart";
+import Contact from "./components/Contact";
+import Pay from "./components/Pay";
+import Order from "./seller/Order";
+import ProductDetail from "./components/ProductDetail";
+import ProductManagement from "./components/seller/ProductManagement";
+import CategoryManagement from "./components/seller/CategoryManagement";
 
 function App() {
   return (
-    <div className='app-container'>
+    <div className="app-container">
       <Header />
       <Routes>
         <Route path="/" element={<Banner />} />
@@ -31,6 +30,7 @@ function App() {
         <Route path="/ProductDetail" element={<ProductDetail />} />
         <Route path="/ProductManagement" element={<ProductManagement />} />
         <Route path="/CategoryManagement" element={<CategoryManagement />} />
+        <Route path="/order" element={<Order />} />
       </Routes>
       <Footer />
     </div>
--- frontend/src/components/page/Header.js
+++ frontend/src/components/page/Header.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React from "react";
 
 function Header() {
   return (
     <header>
       <div className="container py-2">
+        <div className="col-sm-12 col-lg-12 d-flex justify-content-between align-items-center">
+          <div className="main-logo">
+            <a href="/ProductManagement">Kênh người bán</a>
+            <a href="index.html">Trở thành người bán</a>
+          </div>
+
+          <div className="account-options ml-auto">
+            <a href="index.html" className="me-3">
+              Thông báo
+            </a>
+            <a href="index.html" className="me-3">
+              Hỗ trợ
+            </a>
+
+            <div className="custom-select-wrapper me-3">
+              <select className="filter-categoriess">
+                <option>Tiếng việt</option>
+                <option>English</option>
+              </select>
+            </div>
+
+            <a href="index.html" className="me-3">
+              Đăng nhập
+            </a>
+            <a href="index.html">Đăng ký</a>
+          </div>
+        </div>
+
         <div className="row py-4 pb-0 pb-sm-4 align-items-center">
           <div className="col-sm-4 col-lg-3 text-center text-sm-start">
             <div className="main-logo">
@@ -13,144 +41,191 @@ function Header() {
             </div>
           </div>
 
-          <div className="col-sm-6 offset-sm-2 offset-md-0 col-lg-5 d-none d-lg-block">
-            <div className="search-bar border rounded-2 px-3 border-dark-subtle">
+          <div className="col-sm-8 offset-sm-2 offset-md-0 col-lg-7 d-none d-lg-block">
+            <div className="search-bar border rounded-2 px-0 border-dark-subtle">
               <form
                 id="search-form"
-                className="text-center d-flex align-items-center"
+                className="text-center d-flex align-items-center w-100 position-relative"
                 action=""
                 method=""
               >
                 <input
                   type="text"
-                  className="form-control border-0 bg-transparent"
-                  placeholder="Search for more than 10,000 products"
+                  className="form-control"
+                  placeholder="Search products"
+                  style={{ paddingRight: "40px" }} // Tạo khoảng cách bên phải cho icon
                 />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
+                <button
+                  type="submit"
+                  className="btn position-absolute search-button"
+                  style={{
+                    left: "91%",
+                    backgroundColor: "#ff69b4", // Màu hồng cho nút
+                    color: "white", // Màu chữ/icon là trắng
+                    width: "64px", // Chiều rộng của nút
+                    height: "35px", // Chiều cao của nút
+                    border: "none", // Xóa viền
+                    borderRadius: "5px", // Bo góc nút
+                  }}
                 >
-                  <path
-                    fill="currentColor"
-                    d="M21.71 20.29L18 16.61A9 9 0 1 0 16.61 18l3.68 3.68a1 1 0 0 0 1.42 0a1 1 0 0 0 0-1.39ZM11 18a7 7 0 1 1 7-7a7 7 0 0 1-7 7Z"
-                  />
-                </svg>
+                  <i
+                    className="bi bi-search"
+                    style={{ fontSize: "16px", color: "white" }}
+                  ></i>{" "}
+                  {/* Icon tìm kiếm màu trắng */}
+                </button>
               </form>
             </div>
           </div>
 
-          <div className="col-sm-8 col-lg-4 d-flex justify-content-end gap-5 align-items-center mt-4 mt-sm-0 justify-content-center justify-content-sm-end">
-            <div className="support-box text-end d-none d-xl-block">
-              <span className="fs-6 secondary-font text-muted">Phone</span>
-              <h5 className="mb-0">+980-34984089</h5>
-            </div>
-            <div className="support-box text-end d-none d-xl-block">
-              <span className="fs-6 secondary-font text-muted">Email</span>
-              <h5 className="mb-0">waggy@gmail.com</h5>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="container-fluid">
-        <hr className="m-0" />
-      </div>
-
-      <div className="container">
-        <nav className="main-menu d-flex navbar navbar-expand-lg">
-          <div className="d-flex d-lg-none align-items-end mt-3">
+          <div className="col-sm-8 col-lg-2 d-flex justify-content-end gap-5 align-items-center mt-4 mt-sm-0 justify-content-center justify-content-sm-end">
             <ul className="d-flex justify-content-end list-unstyled m-0">
               <li>
-                <a href="account.html" className="mx-3">
-                  <span className="fs-4">👤</span>
+                <a href="index.html" className="mx-3">
+                  <span className="fs-4">
+                    <iconify-icon icon="mdi:heart" class="fs-4"></iconify-icon>
+                  </span>
                 </a>
               </li>
               <li>
-                <a href="wishlist.html" className="mx-3">
-                  <span className="fs-4">❤️</span>
-                </a>
-              </li>
-              <li>
-                <a href="#" className="mx-3" data-bs-toggle="offcanvas" data-bs-target="#offcanvasCart" aria-controls="offcanvasCart">
-                  <span className="fs-4 position-relative">🛒</span>
-                  <span className="position-absolute translate-middle badge rounded-circle bg-primary pt-2">03</span>
-                </a>
-              </li>
-              <li>
-                <a href="#" className="mx-3" data-bs-toggle="offcanvas" data-bs-target="#offcanvasSearch" aria-controls="offcanvasSearch">
-                  <span className="fs-4">🔍</span>
+                <a
+                  href="index.html"
+                  className="mx-3"
+                  data-bs-toggle="offcanvas"
+                  data-bs-target="#offcanvasCart"
+                  aria-controls="offcanvasCart"
+                >
+                  <span className="fs-4 position-relative">
+                    <iconify-icon
+                      icon="mdi:cart"
+                      class="fs-4 position-relative"
+                    ></iconify-icon>
+                  </span>
+                  <span className="position-absolute translate-middle badge rounded-circle bg-danger pt-2">
+                    3
+                  </span>
                 </a>
               </li>
             </ul>
           </div>
 
-          <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-            <div className="offcanvas-header justify-content-center">
-              <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-
-            <div className="offcanvas-body justify-content-between">
-              <select className="filter-categories border-0 mb-0 me-5">
-                <option>All Categories</option>
-                <option>Trang phục</option>
-                <option>Thức ăn</option>
-                <option>Phụ kiên</option>
-                <option>Thú cưng</option>
-              </select>
-
-              <ul className="navbar-nav menu-list list-unstyled d-flex gap-md-3 mb-0">
-                <li className="nav-item">
-                  <a href="/" className="nav-link active">Trang chủ</a>
-                </li>
-                
-                <li className="nav-item">
-                  <a href="/About_us" className="nav-link active">Giới thiệu</a>
-                </li>
-                <li className="nav-item">
-                  <a href="/Product" className="nav-link active">Sản phẩm</a>
-                </li>
-                <li className="nav-item">
-                  <a href="/Contact" className="nav-link active">Liên hệ</a>
-                </li>
-                <li className="nav-item">
-                  <a href="index.html" className="nav-link active">Khuyến mãi</a>
-                </li>
-              </ul>
-
-              <div className="d-none d-lg-flex align-items-end">
+          <div className="container">
+            <nav className="main-menu d-flex navbar navbar-expand-lg justify-content-between">
+              <div className="d-flex d-lg-none align-items-end mt-3">
                 <ul className="d-flex justify-content-end list-unstyled m-0">
-                <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" role="button" id="pages" data-bs-toggle="dropdown" aria-expanded="false"><iconify-icon icon="healthicons:person" class="fs-4"></iconify-icon></a>
-                  <ul className="dropdown-menu" aria-labelledby="pages">
-                    <li><a href="index.html" className="dropdown-item">Đăng ký</a></li>
-                    <li><a href="index.html" className="dropdown-item">Đăng nhập</a></li>
-                    <li><a href="index.html" className="dropdown-item">Single Product</a></li>
-                    <li><a href="/Cart" className="dropdown-item">Cart</a></li>
-                    
-                  </ul>
-                </li>
                   <li>
-                    <a href="index.html" className="mx-3">
-                      <span className="fs-4"><iconify-icon icon="mdi:heart" class="fs-4"></iconify-icon></span>
+                    <a href="account.html" className="mx-3">
+                      <span className="fs-4">👤</span>
                     </a>
                   </li>
                   <li>
-                    <a href="index.html" className="mx-3" data-bs-toggle="offcanvas" data-bs-target="#offcanvasCart" aria-controls="offcanvasCart">
-                      <span className="fs-4 position-relative"><iconify-icon icon="mdi:cart" class="fs-4 position-relative"></iconify-icon></span>
-                      <span className="position-absolute translate-middle badge rounded-circle bg-danger pt-2">3</span>
+                    <a href="wishlist.html" className="mx-3">
+                      <span className="fs-4">❤️</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="mx-3"
+                      data-bs-toggle="offcanvas"
+                      data-bs-target="#offcanvasCart"
+                      aria-controls="offcanvasCart"
+                    >
+                      <span className="fs-4 position-relative">🛒</span>
+                      <span className="position-absolute translate-middle badge rounded-circle bg-primary pt-2">
+                        03
+                      </span>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="mx-3"
+                      data-bs-toggle="offcanvas"
+                      data-bs-target="#offcanvasSearch"
+                      aria-controls="offcanvasSearch"
+                    >
+                      <span className="fs-4">🔍</span>
                     </a>
                   </li>
                 </ul>
               </div>
-            </div>
+
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="offcanvas"
+                data-bs-target="#offcanvasNavbar"
+                aria-controls="offcanvasNavbar"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+
+              <div
+                className="offcanvas offcanvas-end"
+                tabIndex="-1"
+                id="offcanvasNavbar"
+                aria-labelledby="offcanvasNavbarLabel"
+              >
+                <div className="offcanvas-header justify-content-center">
+                  <button
+                    type="button"
+                    className="btn-close"
+                    data-bs-dismiss="offcanvas"
+                    aria-label="Close"
+                  ></button>
+                </div>
+
+                <div className="offcanvas-body d-flex justify-content-start">
+                  <select className="filter-categories border-0 mb-0 me-5 fw-bold">
+                    <option>DANH MỤC SẢN PHẨM</option>
+                    <option>Trang phục</option>
+                    <option>Thức ăn</option>
+                    <option>Phụ kiên</option>
+                    <option>Thú cưng</option>
+                  </select>
+
+                  <ul className="navbar-nav menu-list list-unstyled d-flex gap-md-3 mb-0">
+                    <li className="nav-item">
+                      <a href="/" className="nav-link active">
+                        Trang chủ
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a href="/Product" className="nav-link active">
+                        Cửa hàng cho cún
+                      </a>
+                    </li>
+
+                    <li className="nav-item">
+                      <a href="/Product" className="nav-link active">
+                        Cửa hàng cho mèo
+                      </a>
+                    </li>
+
+                    <li className="nav-item">
+                      <a href="/Product" className="nav-link active">
+                        Cửa hàng cho chuột hamster
+                      </a>
+                    </li>
+
+                    <li className="nav-item">
+                      <a href="/Product" className="nav-link active">
+                        Cửa hàng cho thỏ
+                      </a>
+                    </li>
+
+                    <li className="nav-item">
+                      <a href="/Product" className="nav-link active">
+                        Của hàng cho chim vẹt
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
           </div>
-        </nav>
+        </div>
       </div>
     </header>
   );
