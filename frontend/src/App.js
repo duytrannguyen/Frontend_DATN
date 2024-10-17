76badf9a3f37c0528494a53b49cddaed29377e61
DangNKhang <150359621+DangNKhang@users.noreply.github.com>
2024-10-17 16:33:46 +0700

    Header của tràn seller

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,44 @@
-import { Routes, Route } from 'react-router-dom';
+// import { Routes, Route } from 'react-router-dom';
 import './App.scss';
 import './components/css/Product.css';
-import Header from './components/page/Header';
-import Banner from './components/Banner';
-import Footer from './components/page/Footer';
-import About from './components/About_us';
-import Product from './components/Product';
-import Cart from './components/Cart';
-import Contact from './components/Contact';
-import Pay from './components/Pay';
+// import Header from './components/page/Header';
+// import Banner from './components/Banner';
+// import Footer from './components/page/Footer';
+// import About from './components/About_us';
+// import Product from './components/Product';
+// import Cart from './components/Cart';
+// import Contact from './components/Contact';
+// import Pay from './components/Pay';
+import Headerss from './seller/Header';
+import Sidebar from './seller/sidebar';
+import Dashboard from './seller/main';
+// import OrderManagement from './seller/Order';
 
-function App() {
+// function App() {
+//   return (
+//     <div className='app-container'>
+//       <Header />
+//       <Routes>
+//         <Route path="/" element={<Banner />} />
+//         <Route path="/About_us" element={<About />} />
+//         <Route path="/Product" element={<Product />} />
+//         <Route path="/Cart" element={<Cart />} />
+//         <Route path="/Contact" element={<Contact />} />
+//         <Route path="/Pay" element={<Pay />} />
+//       </Routes>
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default App;
+const App = () => {
   return (
-    <div className='app-container'>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Banner />} />
-        <Route path="/About_us" element={<About />} />
-        <Route path="/Product" element={<Product />} />
-        <Route path="/Cart" element={<Cart />} />
-        <Route path="/Contact" element={<Contact />} />
-        <Route path="/Pay" element={<Pay />} />
-      </Routes>
-      <Footer />
+    <div>
+      <Headerss />
+      <Sidebar />
+      <Dashboard />
+      {/* Các component khác của ứng dụng */}
     </div>
   );
 }
