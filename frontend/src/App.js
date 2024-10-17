c2135d01850c983f53c568246f7a655483b5584f
lytran14 <139949514+lytran14@users.noreply.github.com>
2024-10-18 04:09:31 +0700

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 /* eslint-disable react/jsx-pascal-case */
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.scss";
 import "./components/css/Product.css";
@@ -7,8 +8,22 @@ import "./components/css/ProductManagement.css";
 import User_Router from "./router/User_Router";
 import Seller_Router from "./router/Seller_Router";
 // import "./seller/css/style.css";
-
+import Loader from "./components/Loading";
 const App = () => {
+  const [loading, setLoading] = useState(true); // Trạng thái loading
+
+  useEffect(() => {
+    // Giả lập quá trình tải
+    const timer = setTimeout(() => {
+      setLoading(false); // Thay đổi trạng thái loading sau 2 giây
+    }, 2000);
+
+    return () => clearTimeout(timer); // Dọn dẹp timer khi component bị hủy
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
   return (
     <Routes>
       {/* Định tuyến cho giao diện Admin */}
