b5d3a63dcc5a57f58cad3f6cbaff580cc4bcb1a8
lytran14 <139949514+lytran14@users.noreply.github.com>
2024-10-23 02:12:27 +0700

    Update Pay.js

--- frontend/src/components/page/user/Pay.js
+++ frontend/src/components/page/user/Pay.js
@@ -100,162 +100,158 @@ const CheckoutForm = () => {
           </div>
         </div>
 
-        <div className="card">
-          <strong>PHƯƠNG THỨC VẬN CHUYỂN</strong>
-          <hr />
-          <div className="form-check">
+        {/* <div className="card"> */}
+        <strong>PHƯƠNG THỨC VẬN CHUYỂN</strong>
+        <hr />
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="shippingId"
+            id="standardShipping"
+            value="2"
+            defaultChecked
+          />
+          <label className="form-check-label" htmlFor="standardShipping">
+            <strong>Giao hàng tiêu chuẩn: 32.000 VNĐ</strong>
+            <br />
+            <p style={{ fontSize: "small" }}>Thứ 6 - 25/6</p>
+          </label>
+        </div>
+        {/* </div> */}
+
+        {/* <div className="card"> */}
+        <strong>PHƯƠNG THỨC THANH TOÁN</strong>
+        <hr />
+        {["vnPay", "internetbanking", "cashOnDelivery"].map((method, index) => (
+          <div className="form-check" key={method}>
             <input
               className="form-check-input"
               type="radio"
-              name="shippingId"
-              id="standardShipping"
-              value="2"
-              defaultChecked
+              name="paymentMethodId"
+              id={method}
+              value={index + 1}
+              defaultChecked={method === "cashOnDelivery"}
             />
-            <label className="form-check-label" htmlFor="standardShipping">
-              <strong>Giao hàng tiêu chuẩn: 32.000 VNĐ</strong>
-              <br />
-              <p style={{ fontSize: "small" }}>Thứ 6 - 25/6</p>
+            <label className="form-check-label" htmlFor={method}>
+              <i
+                className={`fa-regular fa-${
+                  method === "cashOnDelivery" ? "money-bill-1" : "credit-card"
+                }`}
+                style={{ fontSize: "x-large" }}
+              ></i>
+              Thanh toán{" "}
+              {method === "cashOnDelivery" ? "khi nhận hàng" : method}
             </label>
           </div>
-        </div>
-
-        <div className="card">
-          <strong>PHƯƠNG THỨC THANH TOÁN</strong>
+        ))}
+        {/* </div> */}
+        {/* <div className="card mt-5"> */}
+        <div className="card-body">
+          <strong>KIỂM TRA LẠI ĐƠN HÀNG</strong>
           <hr />
-          {["vnPay", "internetbanking", "cashOnDelivery"].map(
-            (method, index) => (
-              <div className="form-check" key={method}>
-                <input
-                  className="form-check-input"
-                  type="radio"
-                  name="paymentMethodId"
-                  id={method}
-                  value={index + 1}
-                  defaultChecked={method === "cashOnDelivery"}
-                />
-                <label className="form-check-label" htmlFor={method}>
-                  <i
-                    className={`fa-regular fa-${
-                      method === "cashOnDelivery"
-                        ? "money-bill-1"
-                        : "credit-card"
-                    }`}
-                    style={{ fontSize: "x-large" }}
-                  ></i>
-                  Thanh toán{" "}
-                  {method === "cashOnDelivery" ? "khi nhận hàng" : method}
-                </label>
-              </div>
-            )
-          )}
-        </div>
-        <div className="card mt-5">
-          <div className="card-body">
-            <strong>KIỂM TRA LẠI ĐƠN HÀNG</strong>
-            <hr />
-            <div className="col-md-12">
-              {selectedCartItems.length > 0 ? ( // Kiểm tra nếu có sản phẩm trong giỏ hàng
-                selectedCartItems.map((item) => (
-                  <div className="row" key={item.cartItemId}>
-                    <div className="col-md-2">
-                      {item.products.length > 0 ? ( // Kiểm tra xem có sản phẩm không
-                        item.products.map((product) => {
-                          const productImages = images.filter(
-                            (image) => image.productId === product.productId
-                          );
-                          // Lấy hình ảnh đầu tiên nếu có
-                          return (
-                            <div key={product.imageId}>
-                              {productImages.length > 0 ? (
-                                <img
-                                  src={`/images/${productImages[0].imageName}`}
-                                  alt={product.productName}
-                                  className="product-image"
-                                  style={{ width: "80px", height: "80px" }}
-                                />
-                              ) : (
-                                <p>Không có ảnh cho sản phẩm này.</p>
-                              )}
-                            </div>
-                          );
-                        })
-                      ) : (
-                        <p>Không có hình ảnh cho sản phẩm này.</p> // Thông báo nếu không có hình ảnh
-                      )}
-                    </div>
-                    <div className="col-md-5">
-                      <p>{item.products[0].productName}</p>{" "}
-                      {/* Hiển thị tên sản phẩm */}
-                    </div>
-                    <div className="col-md-1">
-                      <p>{item.quantity}</p> {/* Hiển thị số lượng sản phẩm */}
-                    </div>
-                    <div className="col-md-2">
-                      <p>
-                        <span>
-                          {(
-                            item.products[0].price -
-                            (item.products[0].price *
-                              item.products[0].percentDecrease) /
-                              100
-                          ).toLocaleString()}{" "}
-                          VNĐ
-                        </span>
-                      </p>
-                    </div>
-                    <div className="col-md-2">
-                      <p className="text-primary fw-bold">
-                        <span>
-                          {(
-                            (item.products[0].price -
-                              (item.products[0].price *
-                                item.products[0].percentDecrease) /
-                                100) *
-                            item.quantity
-                          ).toLocaleString()}{" "}
-                          VNĐ
-                        </span>
-                      </p>
-                    </div>
+          <div className="col-md-12">
+            {selectedCartItems.length > 0 ? ( // Kiểm tra nếu có sản phẩm trong giỏ hàng
+              selectedCartItems.map((item) => (
+                <div className="row" key={item.cartItemId}>
+                  <div className="col-md-2">
+                    {item.products.length > 0 ? ( // Kiểm tra xem có sản phẩm không
+                      item.products.map((product) => {
+                        const productImages = images.filter(
+                          (image) => image.productId === product.productId
+                        );
+                        // Lấy hình ảnh đầu tiên nếu có
+                        return (
+                          <div key={product.imageId}>
+                            {productImages.length > 0 ? (
+                              <img
+                                src={`/images/${productImages[0].imageName}`}
+                                alt={product.productName}
+                                className="product-image"
+                                style={{ width: "80px", height: "80px" }}
+                              />
+                            ) : (
+                              <p>Không có ảnh cho sản phẩm này.</p>
+                            )}
+                          </div>
+                        );
+                      })
+                    ) : (
+                      <p>Không có hình ảnh cho sản phẩm này.</p> // Thông báo nếu không có hình ảnh
+                    )}
                   </div>
-                ))
-              ) : (
-                <p>Không có sản phẩm nào trong giỏ hàng.</p> // Hiển thị thông báo nếu không có sản phẩm
-              )}
-              <hr />
-            </div>
-          </div>
-        </div>
-        <div className="footer">
-          <div className="card">
-            <div className="container">
-              <div className="col-md-12">
-                <div className="row">
-                  <div className="col-md-6"></div>
-                  <div className="col-md-3">
-                    <p className="text-end">Thành tiền</p>
-                    <p className="text-end">Khuyến mãi</p>
-                    <p className="text-end">
-                      Phí vận chuyển (Giao hàng tiêu chuẩn)
-                    </p>
-                    <p className="text-end fw-bold">Tổng tiền (gồm VAT)</p>
+                  <div className="col-md-5">
+                    <p>{item.products[0].productName}</p>{" "}
+                    {/* Hiển thị tên sản phẩm */}
+                  </div>
+                  <div className="col-md-1">
+                    <p>{item.quantity}</p> {/* Hiển thị số lượng sản phẩm */}
                   </div>
                   <div className="col-md-2">
-                    <p className="text-end">
-                      {/* <span id="itemTotal">
-                      {total && total.toLocaleString()} VNĐ
-                    </span> */}
-                    </p>
-                    {/* <input type="hidden" name="status" value={status || 1} /> */}
-                    <p className="text-end">
-                      <span id="discountAmount">
-                        {/* {discountAmount && discountAmount.toLocaleString()} VNĐ */}
+                    <p>
+                      <span>
+                        {(
+                          item.products[0].price -
+                          (item.products[0].price *
+                            item.products[0].percentDecrease) /
+                            100
+                        ).toLocaleString()}{" "}
+                        VNĐ
                       </span>
                     </p>
-                    <p className="text-end">32.000 VNĐ</p>
-                    <p className="text-end text-primary fw-bold">
-                      {/* <span id="totalAmount">
+                  </div>
+                  <div className="col-md-2">
+                    <p className="text-primary fw-bold">
+                      <span>
+                        {(
+                          (item.products[0].price -
+                            (item.products[0].price *
+                              item.products[0].percentDecrease) /
+                              100) *
+                          item.quantity
+                        ).toLocaleString()}{" "}
+                        VNĐ
+                      </span>
+                    </p>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <p>Không có sản phẩm nào trong giỏ hàng.</p> // Hiển thị thông báo nếu không có sản phẩm
+            )}
+            <hr />
+          </div>
+        </div>
+        {/* </div> */}
+        <div className="footer">
+          {/* <div className="card"> */}
+          <div className="container">
+            <div className="col-md-12">
+              <div className="row">
+                <div className="col-md-6"></div>
+                <div className="col-md-3">
+                  <p className="text-end">Thành tiền</p>
+                  <p className="text-end">Khuyến mãi</p>
+                  <p className="text-end">
+                    Phí vận chuyển (Giao hàng tiêu chuẩn)
+                  </p>
+                  <p className="text-end fw-bold">Tổng tiền (gồm VAT)</p>
+                </div>
+                <div className="col-md-2">
+                  <p className="text-end">
+                    {/* <span id="itemTotal">
+                      {total && total.toLocaleString()} VNĐ
+                    </span> */}
+                  </p>
+                  {/* <input type="hidden" name="status" value={status || 1} /> */}
+                  <p className="text-end">
+                    <span id="discountAmount">
+                      {/* {discountAmount && discountAmount.toLocaleString()} VNĐ */}
+                    </span>
+                  </p>
+                  <p className="text-end">32.000 VNĐ</p>
+                  <p className="text-end text-primary fw-bold">
+                    {/* <span id="totalAmount">
                       {(
                         (total || 0) -
                         (discountAmount || 0) +
@@ -263,53 +259,52 @@ const CheckoutForm = () => {
                       ).toLocaleString()}{" "}
                       VNĐ
                     </span> */}
-                    </p>
-                  </div>
-                  <div className="col-md-1"></div>
+                  </p>
                 </div>
+                <div className="col-md-1"></div>
               </div>
+            </div>
 
-              <hr />
-              <div className="col-md-12">
-                <div className="row">
-                  <div className="col-md-7">
-                    <div className="form-check">
-                      <input
-                        className="form-check-input"
-                        type="checkbox"
-                        id="agreeTerms"
-                      />
-                      <label className="form-check-label" htmlFor="agreeTerms">
-                        Bằng việc xác nhận mua hàng, bạn đã hoàn toàn đồng ý với
-                        <p>
-                          <a
-                            href="/"
-                            className="link-danger link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover fw-bold"
-                          >
-                            Điều khoản & Điều kiện
-                          </a>{" "}
-                          của chúng tôi.
-                        </p>
-                      </label>
-                    </div>
-                  </div>
-                  <div className="col-md-5 d-flex justify-content-center align-items-center">
-                    <button
-                      type="submit"
-                      id="confirmBtn"
-                      className="btn btn-outline-primary my-0"
+            {/* </div> */}
+          </div>
+        </div>
+        <br />
+        <hr />
+        <div className="col-md-12">
+          <div className="row">
+            <div className="col-md-7">
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  id="agreeTerms"
+                />
+                <label className="form-check-label" htmlFor="agreeTerms">
+                  Bằng việc xác nhận mua hàng, bạn đã hoàn toàn đồng ý với
+                  <p>
+                    <a
+                      href="/"
+                      className="link-danger link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover fw-bold"
                     >
-                      XÁC NHẬN ĐẶT HÀNG
-                    </button>
-                  </div>
-                </div>
+                      Điều khoản & Điều kiện
+                    </a>{" "}
+                    của chúng tôi.
+                  </p>
+                </label>
               </div>
+            </div>
+            <div className="col-md-5 d-flex justify-content-center align-items-center">
+              <button
+                type="submit"
+                id="confirmBtn"
+                className="btn btn-outline-primary my-0"
+              >
+                XÁC NHẬN ĐẶT HÀNG
+              </button>
             </div>
           </div>
         </div>
       </div>
-
-      <br />
     </form>
   );
 };
